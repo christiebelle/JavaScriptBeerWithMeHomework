9e88b3e9159b51a3d8194846a98828c0f6ac28d4
Christiebelle <cslester@outlook.com>
2018-04-11 16:02:02 +0100

    api url added in

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
-var app = function(){
+const app = function(){
 
-}
+    const api = "https://api.punkapi.com/v2/beers";
+    makeRequest(api, requestComplete);
+};
 
 window.addEventListener('load', app);
